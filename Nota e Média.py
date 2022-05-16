0916b89fca8d821b2722cd6552980355c8a89d26
pedrovi34 <103965597+pedrovi34@users.noreply.github.com>
2022-05-16 15:19:59 -0300

    Add files via upload

---
+++ Nota e Média.py
@@ -0,0 +1,5 @@
+n1 = str(input('Digite a primeira nota'))
+n2 = str(input('digite a segunda nota'))
+s = n1+n2
+m = s/2
+print('A soma de {n1}, e {n2} é igual a {s}, então a média é {m}'.format(n1,n2,s,m))
